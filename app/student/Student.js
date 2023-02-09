c410e967e0efa5546c70cf3c5c4a34acd1baa023
Manav Misra <manavm@visionwebsoft.com>
2023-02-09 00:13:08 -0600

    Set Student to not have versionKey

--- app/student/Student.js
+++ app/student/Student.js
@@ -2,11 +2,14 @@ import { model, mongoose } from "mongoose";
 import AttendanceSchema from "./attendance-schema.js";
 import GradeSchema from "./grade-schema.js";
 
-export const studentSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  attendance: [AttendanceSchema],
-  grades: [GradeSchema],
-});
+export const studentSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    attendance: [AttendanceSchema],
+    grades: [GradeSchema],
+  },
+  { versionKey: false }
+);
 
 export default model("Student", studentSchema);
